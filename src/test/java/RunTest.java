81225d53e8d4beea08d5ea21803588032fe4c8c2
Zlobin Fedor <zlobin.f@gmail.com>
2024-09-07 11:56:40 +0300

    fix: исправил порядок выполнения тестов
    JUnit запускал тесты в алфавитно-числовом порядке, поэтому failingTest запускался перед passingTest, исправил с помощбю аннотации класса @TestMethodOrder и методов @Order

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,3 +1,39 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+
+/**
+ * Test runner
+ */
+@TestMethodOrder(OrderAnnotation.class)
 public class RunTest {
-    //its RunTest origin
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Starting tests");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Tests finished");
+    }
+
+    @Test
+    @Order(1)
+    public void passingTest() {
+        System.out.println("Running passing test");
+        Assertions.assertEquals("Java Rocks!", "Java Rocks!");
+    }
+
+    @Test
+    @Order(2)
+    public void failingTest() {
+        System.out.println("Running failing test");
+        Assertions.assertEquals("Java Rocks!", "Java Sucks!");
+    }
 }
