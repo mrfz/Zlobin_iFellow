fcff4cbf39bd3cf245c03f1e71b60e69174e6515
Fedor Zlobin <zlobin.f@gmail.com>
2024-09-07 11:37:10 +0300

    feat: реализовал два теста в RunTest.java

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,3 +1,33 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Test runner
+ */
 public class RunTest {
-    //its RunTest origin
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Starting tests");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Tests finished");
+    }
+
+    @Test
+    public void passingTest() {
+        System.out.println("Running passing test");
+        Assertions.assertEquals("Java Rocks!", "Java Rocks!");
+    }
+
+    @Test
+    public void failingTest() {
+        System.out.println("Running failing test");
+        Assertions.assertEquals("Java Rocks!", "Java Sucks!");
+    }
 }
