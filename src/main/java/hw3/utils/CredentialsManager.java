5deaa63c8d6831a2258c16527e0741667f14c454
Zlobin Fedor <zlobin.f@gmail.com>
2024-09-17 01:29:42 +0300

    docs: добавил информацию об учетных данных в README

---
+++ src/main/java/hw3/utils/CredentialsManager.java
@@ -0,0 +1,18 @@
+package hw3.utils;
+
+/**
+ * Класс для хранения учетных данных
+ * в репозитории хранится с данными по умолчанию,
+ * для работы в приложении необходимо изменить возвращаемые параметры
+ *
+ */
+public class CredentialsManager {
+    public static String getUsername() {
+        return "AT2";
+    }
+
+    public static String getPassword() {
+        return "Qwerty123";
+    }
+}
+
