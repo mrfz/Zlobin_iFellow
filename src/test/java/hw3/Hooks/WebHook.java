b19740ec08d052a6ce34af9e704833ddf5e5a709
Zlobin Fedor <zlobin.f@gmail.com>
2024-10-08 04:45:29 +0300

    fix: изменил BeforeAll на BeforeEach для подготовки веб драйвера

--- src/test/java/hw3/Hooks/WebHook.java
+++ src/test/java/hw3/Hooks/WebHook.java
@@ -3,17 +3,21 @@ package hw3.Hooks;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import hw3.utils.ConfigurationManager;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class WebHook {
-    @BeforeAll
-    public static void setUp() {
+
+    @Step("Подготовка веб-драйвера")
+    @BeforeEach
+    public void setUp() {
 
         WebDriverRunner.setWebDriver(ConfigurationManager.getWebDriver());
 
     }
 
+    @Step("Закрытие веб-драйвера")
     @AfterEach
     public void tearDown() {
         Selenide.closeWebDriver();
