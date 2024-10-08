b497e65f666489c0bf2e0f0122777187813e4276
Zlobin Fedor <zlobin.f@gmail.com>
2024-10-08 05:23:38 +0300

    feat: добавил аннотации к селекторам

--- src/main/java/hw3/components/IssueCreationForm.java
+++ src/main/java/hw3/components/IssueCreationForm.java
@@ -7,15 +7,15 @@ import org.openqa.selenium.Keys;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class IssueCreationForm {
-    private final SelenideElement issueForm = $x("//section[@id='create-issue-dialog']");
-    private final SelenideElement issueTitle = issueForm.$x(".//input[@id='summary']");
-    private final SelenideElement issueDescription = issueForm.$x(".//textarea[@id='description']");
-    private final SelenideElement issueFixInVersion = issueForm.$x(".//select[@name='fixVersions']//option[contains(text(),'Version 2.0')]");
-    private final SelenideElement issueSubmitButton = issueForm.$x(".//input[@id='create-issue-submit']");
-    private final SelenideElement issuePriority = issueForm.$x(".//div[@id='priority-single-select']/input");
-    private final SelenideElement issueLabel = issueForm.$x(".//div[@id='labels-multi-select']");
-    private final SelenideElement issueVersionAffected = issueForm.$x(".//select[@id='versions']/optgroup/option[contains(text(),'Version 1.0')]");
-    private final SelenideElement issueSeverity = issueForm.$x(".//label[contains(text(), 'Серьезность')]/following-sibling::select/option[contains(text(),'S0 Тривиальный/Trivial')]");
+    private final SelenideElement issueForm = $x("//section[@id='create-issue-dialog']").as("Форма создания задачи");
+    private final SelenideElement issueTitle = issueForm.$x(".//input[@id='summary']").as("Название задачи");
+    private final SelenideElement issueDescription = issueForm.$x(".//textarea[@id='description']").as("Описание задачи");
+    private final SelenideElement issueFixInVersion = issueForm.$x(".//select[@name='fixVersions']//option[contains(text(),'Version 2.0')]").as("Исправление в версии");
+    private final SelenideElement issueSubmitButton = issueForm.$x(".//input[@id='create-issue-submit']").as("Кнопка создания задачи");
+    private final SelenideElement issuePriority = issueForm.$x(".//div[@id='priority-single-select']/input").as("Приоритет задачи");
+    private final SelenideElement issueLabel = issueForm.$x(".//div[@id='labels-multi-select']").as("Теги задачи");
+    private final SelenideElement issueVersionAffected = issueForm.$x(".//select[@id='versions']/optgroup/option[contains(text(),'Version 1.0')]").as("Подвергаемые версии");
+    private final SelenideElement issueSeverity = issueForm.$x(".//label[contains(text(), 'Серьезность')]/following-sibling::select/option[contains(text(),'S0 Тривиальный/Trivial')]").as("Серьезность задачи");
 
 
     public void createIssue(String title, String description) {
--- src/main/java/hw3/pages/JiraTask.java
+++ src/main/java/hw3/pages/JiraTask.java
@@ -6,11 +6,11 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class JiraTask {
-    private final SelenideElement taskStatus = $x("//span[@id='status-val']/span");
-    private final SelenideElement taskFixVersion = $x("//span[@id='fixVersions-field']/a");
-    private final SelenideElement taskInProgress = $x("//span[@class='trigger-label' and contains(text(), 'В работе')]");
-    private final SelenideElement taskProcess = $x("//span[@class='dropdown-text' and contains(text(), 'Бизнес-процесс')]");
-    private final SelenideElement taskDoneSelection = $x("//aui-dropdown-menu[@id='opsbar-transitions_more_drop']//span[contains(text(), 'Выполнено')]/parent::a/parent::aui-item-link");
+    private final SelenideElement taskStatus = $x("//span[@id='status-val']/span").as("Статус задачи");
+    private final SelenideElement taskFixVersion = $x("//span[@id='fixVersions-field']/a").as("Исправить в версии");
+    private final SelenideElement taskInProgress = $x("//span[@class='trigger-label' and contains(text(), 'В работе')]").as("Задача в работе");
+    private final SelenideElement taskProcess = $x("//span[@class='dropdown-text' and contains(text(), 'Бизнес-процесс')]").as("Бизнес-процесс задачи");
+    private final SelenideElement taskDoneSelection = $x("//aui-dropdown-menu[@id='opsbar-transitions_more_drop']//span[contains(text(), 'Выполнено')]/parent::a/parent::aui-item-link").as("Завершение задачи");
 
     public String getTaskStatus() {
         return taskStatus.getText();
