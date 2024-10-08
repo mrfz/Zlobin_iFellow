d7ee3a12fcfce676d045ab383b77125562aa55ea
Zlobin Fedor <zlobin.f@gmail.com>
2024-10-08 11:07:14 +0300

    fix: очистил от неиспользуемых элементов, методов

--- src/main/java/hw3/components/JiraHeader.java
+++ src/main/java/hw3/components/JiraHeader.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.Selenide.$x;
 public class JiraHeader {
     private final SelenideElement navBarPrimary = $x("//div[@class='aui-header-primary']").as("Основная навигационная панель");
     private final SelenideElement navBarSecondary = $x("//div[@class='aui-header-secondary']").as("Вспомогательная навигационная панель");
-    private final SelenideElement userOptions = $x("//li[@id='user-options']").as("Пользовательские настройки");
     private final SelenideElement logoutButton = $x("//li[@id='user-options']//a[@id='log_out']").as("Кнопка выхода");
     private final SelenideElement projectsMenu = navBarPrimary.$x(".//a[text()='Проекты']").as("Меню проектов");
     private final SelenideElement allProjectsLink = navBarPrimary.$x(".//a[@id='project_view_all_link_lnk']").as("Ссылка на все проекты");
@@ -17,8 +16,8 @@ public class JiraHeader {
     private final SelenideElement searchDropdownResults = navBarSecondary.$x(".//div[@class='quicksearch-dropdown']").as("Результаты поиска");
 
 
-    public boolean logoutButtonExists() {
-        return logoutButton.shouldBe(Condition.exist).exists();
+    public SelenideElement logoutButtonExists() {
+        return logoutButton.shouldBe(Condition.exist);
     }
 
     public void openProjectsDashboard() {
