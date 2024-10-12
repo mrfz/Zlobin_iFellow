3ebe88c48332f075f7e9670ceb8c5e0f3eba9b28
Zlobin Fedor <zlobin.f@gmail.com>
2024-10-12 15:43:15 +0300

    docs: добавил описание классов и методов пакета pageObjects

--- src/main/java/ru/iFellow/pages/JiraTask.java
+++ src/main/java/ru/iFellow/pages/JiraTask.java
@@ -8,6 +8,10 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$x;
 
+/**
+ * Страница задачи в Jira
+ * @author Fedor Zlobin
+ */
 public class JiraTask {
     private final SelenideElement taskStatus = $x("//span[@id='status-val']/span").as("Статус задачи");
     private final SelenideElement taskFixVersion = $x("//span[@id='fixVersions-field']/a").as("Исправить в версии");
@@ -15,22 +19,42 @@ public class JiraTask {
     private final SelenideElement taskProcess = $x("//span[@class='dropdown-text' and contains(text(), 'Бизнес-процесс')]").as("Бизнес-процесс задачи");
     private final SelenideElement taskDoneSelection = $x("//aui-dropdown-menu[@id='opsbar-transitions_more_drop']//span[contains(text(), 'Выполнено')]/parent::a/parent::aui-item-link").as("Завершение задачи");
 
+    /**
+     * Получить элемент статуса задачи
+     * @return <code>SelenideElement</code> - элемент статуса задачи
+     */
     public SelenideElement getTaskStatus() {
         return taskStatus;
     }
 
+    /**
+     * Получить элемент исправить в версии задачи
+     * @return <code>SelenideElement</code> - элемент исправить в версии задачи
+     */
     public String getFixVersion() {
         return taskFixVersion.getText();
     }
 
+    /**
+     * Перевести задачу в работу
+     */
     public void moveTaskToInProgress() {
         taskInProgress.click();
     }
 
+    /**
+     * Получить элемент кнопки бизнес-процесса задачи
+     * @return <code>SelenideElement</code> - элемент кнопки бизнес-процесса задачи
+     */
     public SelenideElement getTaskProcess() {
         return taskProcess;
     }
 
+    /**
+     * Получить элемент кнопки завершения задачи
+     * выполняется проверка видимости элемента с задержкой 3 секунды
+     * @return <code>SelenideElement</code> - элемент кнопки завершения задачи
+     */
     public SelenideElement getTaskDoneSelection() {
         return taskDoneSelection.shouldBe(Condition.visible, Duration.ofSeconds(3));
     }
